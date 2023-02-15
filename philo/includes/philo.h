5c9822ee8569692a330c4264fc88a5d21c37443b
takira <ca.fie.0220@gmail.com>
2023-02-15 19:26:37 +0900

    Add outline

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 19:32:01 by takira            #+#    #+#             */
-/*   Updated: 2022/12/30 19:32:03 by takira           ###   ########.fr       */
+/*   Updated: 2023/02/09 18:51:01 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,16 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
+# include <pthread.h>
 
 # include <unistd.h>
 
-# define PASS	1
-# define FAIL	0
+# define SUCCESS		0
+# define FAILURE		1
+# define PROCESS_ERROR	2
+
+# define PHILO_DEAD		0
+# define PHILO_ALIVE	1
 
 # define NUM_OF_PHILOS_IDX		1
 # define TIME_TO_DIE_IDX		2
@@ -31,22 +36,37 @@
 # define TIME_TO_SLEEP_IDX		4
 # define MUST_EAT_TIMES_IDX		5
 
+# define CHECK_TIME_SPAM_MS		100
+
 # define ERROR_MSG_NUM_OF_PHILOS	"1 <= NumOfPhilos <= 200"
 # define ERROR_MSG_TIME_TO_DIE		"60 <= TimeToDie"
 # define ERROR_MSG_TIME_TO_EAT		"60 <= TimeToEat"
 # define ERROR_MSG_TIME_TO_SLEEP	"60 <= TimeToSleep"
 # define ERROR_MSG_MUST_EAT_TIMES	"0 <= MustEatTimes"
 
-typedef struct s_philo		t_info;
+typedef struct s_args		t_args;
+typedef struct s_philo		t_philo;
 typedef enum s_input_type	t_input_type;
 
-struct s_philo
+struct s_args
 {
 	int	num_of_philo;
 	int	time_to_die;
 	int time_to_eat;
 	int time_to_sleep;
 	int must_eat_times;
+};
+
+struct s_philo
+{
+	// philo
+	pthread_t	*idx;
+
+	// fork
+
+
+	// waiter
+
 };
 
 enum s_input_type
@@ -58,10 +78,12 @@ enum s_input_type
 	E_MUST_EAT_TIMES
 };
 
+/* philo */
+
 /* libs */
-int	ft_atoi(const char *str, bool *is_success);
-char	*ft_strchr(const char *s, int c);
-size_t	ft_strlen_ns(const char *s);
+int		ft_atoi(const char *str, bool *is_success);
 int		ft_isdigit(int c);
+size_t	ft_strlen_ns(const char *s);
+char	*ft_strchr(const char *s, int c);
 
 #endif
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,28 +6,25 @@
 /*   By: takira <takira@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 19:32:16 by takira            #+#    #+#             */
-/*   Updated: 2023/02/09 17:59:20 by takira           ###   ########.fr       */
+/*   Updated: 2023/02/09 19:07:45 by takira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-t_info	*init_info(void)
+t_args	*init_args(void)
 {
-	t_info	*info;
+	t_args	*args;
 
-	info = (t_info *)malloc(sizeof(t_info));
-	if (!info)
+	args = (t_args *)malloc(sizeof(t_args));
+	if (!args)
 		return (NULL);
-	info->num_of_philo = 0;
-	info->time_to_die = 0;
-	info->time_to_eat = 0;
-	info->time_to_sleep = 0;
-	info->must_eat_times = 0;
-	return (info);
+	memset(args, 0, sizeof(*args));
+
+	return (args);
 }
 
-int	msg_and_return_num(char *msg, int ret_num)
+int	print_msg_ret_int(char *msg, int ret_num)
 {
 	if (msg)
 		printf("%s\n", msg);
@@ -49,50 +46,132 @@ int	check_num_valid(int num, t_input_type type)
 	return (0);
 }
 
-int	get_input(int argc, char **argv, t_info *info)
+int	get_input_args(char **argv, t_args *args)
 {
 	bool	is_atoi_success;
 
-	info->num_of_philo = ft_atoi(argv[NUM_OF_PHILOS_IDX], &is_atoi_success);
-	if (!is_atoi_success && check_num_valid(info->num_of_philo, E_NUM_OF_PHILOS))
-		return (msg_and_return_num(ERROR_MSG_NUM_OF_PHILOS, FAIL));//TODO: num_of_philo = 1
-	info->time_to_die = ft_atoi(argv[TIME_TO_DIE_IDX], &is_atoi_success);
-	if (!is_atoi_success && check_num_valid(info->time_to_die, E_TIME_TO_DIE))
-		return (msg_and_return_num(ERROR_MSG_TIME_TO_DIE, FAIL));
-	info->time_to_eat = ft_atoi(argv[TIME_TO_EAT_IDX], &is_atoi_success);
-	if (!is_atoi_success && check_num_valid(info->time_to_eat, E_TIME_TO_EAT))
-		return (msg_and_return_num(ERROR_MSG_TIME_TO_EAT, FAIL));
-	info->time_to_sleep = ft_atoi(argv[TIME_TO_SLEEP_IDX], &is_atoi_success);
-	if (!is_atoi_success && check_num_valid(info->time_to_sleep, E_TIME_TO_SLEEP))
-		return (msg_and_return_num(ERROR_MSG_TIME_TO_SLEEP, FAIL));
-	if (argc == MUST_EAT_TIMES_IDX)
+	args->num_of_philo = ft_atoi(argv[NUM_OF_PHILOS_IDX], &is_atoi_success);
+	if (!is_atoi_success && check_num_valid(args->num_of_philo, E_NUM_OF_PHILOS))
+		return (print_msg_ret_int(ERROR_MSG_NUM_OF_PHILOS, FAILURE));//TODO: num_of_philo = 1
+	args->time_to_die = ft_atoi(argv[TIME_TO_DIE_IDX], &is_atoi_success);
+	if (!is_atoi_success && check_num_valid(args->time_to_die, E_TIME_TO_DIE))
+		return (print_msg_ret_int(ERROR_MSG_TIME_TO_DIE, FAILURE));
+	args->time_to_eat = ft_atoi(argv[TIME_TO_EAT_IDX], &is_atoi_success);
+	if (!is_atoi_success && check_num_valid(args->time_to_eat, E_TIME_TO_EAT))
+		return (print_msg_ret_int(ERROR_MSG_TIME_TO_EAT, FAILURE));
+	args->time_to_sleep = ft_atoi(argv[TIME_TO_SLEEP_IDX], &is_atoi_success);
+	if (!is_atoi_success && check_num_valid(args->time_to_sleep, E_TIME_TO_SLEEP))
+		return (print_msg_ret_int(ERROR_MSG_TIME_TO_SLEEP, FAILURE));
+	if (argv[MUST_EAT_TIMES_IDX])
 	{
-		info->must_eat_times = ft_atoi(argv[MUST_EAT_TIMES_IDX], &is_atoi_success);
-		if (!is_atoi_success && check_num_valid(info->must_eat_times, E_MUST_EAT_TIMES))
-			return (msg_and_return_num(ERROR_MSG_MUST_EAT_TIMES, FAIL));//TODO: must_eat_times = 0
+		args->must_eat_times = ft_atoi(argv[MUST_EAT_TIMES_IDX], &is_atoi_success);
+		if (!is_atoi_success && check_num_valid(args->must_eat_times, E_MUST_EAT_TIMES))
+			return (print_msg_ret_int(ERROR_MSG_MUST_EAT_TIMES, FAILURE));//TODO: must_eat_times = 0
 	}
-	return (PASS);
+	return (SUCCESS);
+}
+
+t_philo	*init_philo(t_args *args)
+{
+	t_philo	*philo;
+
+	if (!args)
+		return (NULL);
+
+
+	return (philo);
+}
+
+int	activate_philo(t_args *args, t_philo *philo)
+{
+
+}
+
+void	*start_each_philo(void *philo)
+{
+	// check for waiter
+
+	// take fork & print
+
+	// eat & print
+
+	// release fork
+
+	// sleep & print
+
+	// think & print
+
+}
+
+int	check_philo_dead(t_args *args, t_philo *philo)
+{
+	size_t	idx;
+
+	idx = 0;
+	while (idx < args->num_of_philo)
+	{
+
+		idx++;
+	}
+}
+
+int	start_spaghetti_party(t_args *args, t_philo *philo)
+{
+	size_t	idx;
+
+	idx = 0;
+	while (idx < args->num_of_philo)
+	{
+		if (pthread_create(&philo->idx[idx], NULL, start_each_philo, philo) != SUCCESS)
+			return (PROCESS_ERROR);
+		if (pthread_join(philo->idx[idx], NULL) != SUCCESS)
+			return (PROCESS_ERROR);
+		idx++;
+	}
+	while (true)
+	{
+		if (check_philo_dead(args, philo) == PHILO_DEAD)
+			break ;
+		usleep(CHECK_TIME_SPAM_MS); // check time span
+	}
+	return (SUCCESS);
+}
+
+void	free_allocs(t_args *args)
+{
+
 }
 
 int	main(int argc, char **argv)
 {
-	t_info	*info;
+	t_args	*args;
+	t_philo	*philo;
 
 	if (argc < 5 || 6 < argc)
 	{
 		printf("[Error] Invalid argument. Input as following:\n" \
-		"$>./philo NumOfPhilos(1~200) TimeToDie(60~) TimeToEat(60~) TimeToSleep(60~) <MustEatTimes(0~)>\n");
+		"$>./args NumOfPhilos(1~200) TimeToDie(60~) " \
+		"TimeToEat(60~) TimeToSleep(60~) <OP>MustEatTimes(0~)\n");
 		return (EXIT_FAILURE);
 	}
-	info = init_info();
-	if (!info)
+	args = init_args();
+	if (!args)
 	{
 		printf("[Error] Fail to malloc\n");
 		return (EXIT_FAILURE);
 	}
-	if (get_input(argc, argv, info) == FAIL)
+	if (get_input_args(argv, args) == FAILURE)
 		return (EXIT_FAILURE);
-	printf("NumOfPhilos:%d, TimeToDie:%d, TimeToEat:%d, TimeToSleep:%d, MustEatTimes:%d\n", info->num_of_philo, info->time_to_die, info->time_to_eat, info->time_to_sleep, info->must_eat_times);
+	printf("[#DEBUG]NumOfPhilos:%d, TimeToDie:%d, TimeToEat:%d, TimeToSleep:%d, MustEatTimes:%d\n", args->num_of_philo, args->time_to_die, args->time_to_eat, args->time_to_sleep, args->must_eat_times);
 
-	return (0);
+	philo = init_philo(args);
+	if (!philo)
+	{
+		printf("[Error] Fail to malloc\n");
+		return (EXIT_FAILURE);
+	}
+	if (start_spaghetti_party(args, philo) == PROCESS_ERROR)
+		return (EXIT_FAILURE);
+	free_allocs(args);
+	return (EXIT_SUCCESS);
 }
